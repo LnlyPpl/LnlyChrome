65ccb41b4e58590f6914b366f36d7677a840dfb8
dflass <dflass@crimson.ua.edu>
2017-03-25 16:02:08 -0500

    Fix dat extenssion
    Fixin ish.

---
+++ popup.js
@@ -0,0 +1,29 @@
+
+function onOff(){
+    var x = $("#slider").is(':checked');
+    if(x){
+        $("#s1").text("On")
+    }
+    else{
+        $("#s1").text("Off")
+    }
+}
+
+function updateActiveStatus(isActive){
+    if(isActive){
+
+        $("#slider").prop('checked', true);
+        $("#s1").text("On")
+    }
+    else{
+        $("#s1").text("Off")
+    }
+}
+
+window.onload=function(){
+    $("#slider").prop('checked', true);
+    $("#s1").text("On")
+    chrome.storage.local.get("active", updateActiveStatus)
+
+    $('#slider').on("change", onOff);
+}
